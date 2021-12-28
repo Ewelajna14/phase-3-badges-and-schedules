d38906d935d1ba3b6be32a609979e1b330412188
Ewelina Kowalczyk <ekowalczyk1994@gmail.com>
2021-12-28 13:37:04 -0600

    commit tree 598444f983435ee888af1db31ffa3c2f32bceec0

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}." 
+end 
+
+def batch_badge_creator(new_array)
+new_array.map do |name|
+    "Hello, my name is #{name}."
+end 
+end 
+
+def assign_rooms(new_array)
+new_array.map.with_index do |name, index|
+    index +=1
+     "Hello, #{name}! You'll be assigned to room #{index}!"
+end
+end
+
+def printer(batch_badge_creator, assign_rooms)
+    batch_badge_creator.each do |argument|
+    end
+    puts argument
+    assign_rooms.each do |room|
+    puts room
+    end
+end
